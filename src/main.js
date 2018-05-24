3d3358d56ed3472826cfc317788cc55fed03bbab
mahuanfy <mahuanfy@foxmail.com>
2018-05-24 23:02:57 +0800

    重构代码第一次

--- src/main.js
+++ src/main.js
@@ -1,19 +1,23 @@
 'use strict'
-let TOP_NUMBER  =   ["._.","...","._.","._.","...","._.","._.","._.","._.","._."]
-let CENTRE_NUMBER = ["|.|","..|","._|","._|","|_|","|_.","|_.","..|","|_|","|_|"]
-let BOTTOM_NUMBER = ["|_|","..|","|_.","._|","..|","._|","|_|","..|","|_|","..|"]
+let TOP_NUMBER = ["._.", "...", "._.", "._.", "...", "._.", "._.", "._.", "._.", "._."]
+let CENTRE_NUMBER = ["|.|", "..|", "._|", "._|", "|_|", "|_.", "|_.", "..|", "|_|", "|_|"]
+let BOTTOM_NUMBER = ["|_|", "..|", "|_.", "._|", "..|", "._|", "|_|", "..|", "|_|", "..|"]
 
-module.exports =  function number (num){
+module.exports = function number(num) {
+
+    return ArrayToString(num);
+}
+
+function ArrayToString(num) {
     let numbers = num.split('');
     let numStr1 = [];
     let numStr2 = [];
     let numStr3 = [];
-    numbers.map(item =>{
+
+    numbers.map(item => {
         numStr1.push(TOP_NUMBER[item])
         numStr2.push(CENTRE_NUMBER[item])
         numStr3.push(BOTTOM_NUMBER[item])
     })
-    let numStr = numStr1.join(" ")+"\n"+numStr2.join(" ")+"\n"+numStr3.join(" ")+"\n"
-    console.log(numStr)    
-    return numStr;
+    return numStr1.join(" ") + "\n" + numStr2.join(" ") + "\n" + numStr3.join(" ") + "\n"
 }
